ada28de95101a6b5d807a70790b9b5503ddf0719
wls <wushi91@126.com>
2017-12-19 00:01:01 +0800

    小改善

--- src/pages/base/QRTable.vue
+++ src/pages/base/QRTable.vue
@@ -1,76 +1,38 @@
 <template>
   <div class="qr-table">
 
+
     <div class="table-header">
-
-
-            <span class="header-item" v-for="item in headerData"
-                  :style="{ 'width': item.width}">{{ item.label }}</span>
-
-
-      <!--<span>房源名称</span>-->
-      <!--<span>租客</span>-->
-
+      <span class="header-item" v-for="item in headerData"
+                 :style="{'width':item.width}">{{ item.label }}</span>
     </div>
 
     <div class="table-content">
-
-
       <div class="table-row" v-for="item in tableData">
-
         <div class='table-house-use' v-if="!item.isUnused">
 
           <!--有租客的房源-->
           <span v-for="header in headerData" :style="{ 'width': header.width}">{{ item[header.prob] }}</span>
-          <!--:title="item.houseName"-->
-          <!--v-model="more_action_popover_visible"-->
-          <!--width="200"-->
-          <!--content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"-->
-          <!--<el-popover-->
-            <!--placement="top"-->
-            <!--width="150"-->
-            <!--v-model="item.id === deleteId"-->
-            <!--trigger="click">-->
-            <!--<div style="text-align: center; margin: auto">-->
-              <!--<el-button size="mini" type="text" @click="deleteId = deleteIdDefault">取消</el-button>-->
-              <!--<el-button type="primary" size="mini" @click="deleteId = deleteIdDefault">确定</el-button>-->
-            <!--</div>-->
-            <!--<el-button slot="reference" @click="deleteAccountConfim(item.id)">删除{{item.id}}</el-button>-->
-          <!--</el-popover>-->
-
-
 
           <el-popover
             placement="bottom"
             width="150"
             trigger="hover">
-            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;"  @click="editAccount">编辑账本</el-button>
-            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="toRenterDetail">租客详情</el-button>
+            <el-button slot="reference" class="more-action">更多</el-button>
 
+            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="editAccount">编辑账本
+            </el-button>
+            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="toRenterDetail">
+              租客详情
+            </el-button>
+            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="deleteAccount">
+              删除账本
+            </el-button>
 
-            <el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="deleteAccountConfim(item.id)">删除账本</el-button>
-            <div style="text-align: center; margin: auto" v-show="item.id === deleteId">
-              <el-button size="mini" type="primary" @click="deleteId = deleteIdDefault">取消</el-button>
-              <el-button type="primary" size="mini" @click="deleteId = deleteIdDefault">确定</el-button>
-            </div>
-
-
-            <!--<el-popover-->
-              <!--placement="right" width="150" trigger="click">-->
-              <!--<div style="text-align: center; margin: auto">-->
-                <!--<el-button size="mini" type="text" @click="visible2 = false">取消</el-button>-->
-                <!--<el-button type="primary" size="mini" @click="visible2 = false">确定</el-button>-->
-              <!--</div>-->
-              <!--<el-button slot="reference">删除</el-button>-->
-            <!--</el-popover>-->
-
-            <el-button class="more-action" slot="reference">更多</el-button>
           </el-popover>
-          <!--<span  >更多</span>-->
 
         </div>
         <div class='table-house-unuse' v-else>
-
           <!--没有租客的房源-->
           <span :style="{ 'width': headerData[0].width}">{{ item[headerData[0].prob]}}</span>
           <span style="color:rgba(242,73,73,1);cursor: pointer;" @click="addRenter">添加租客信息</span>
@@ -90,47 +52,53 @@
 
   export default {
     name: "QRTable",
+    props:['isTypeAll','isTypeUnused','isOutdate'],
     data() {
       return {
-        deleteIdDefault:'00s神udfijsdoifhify342',
-        deleteId:this.deleteIdDefault,
-        more_action_popover_visible:false,
-
-
-        headerData: [
+        allAccountHeaderData: [
           {label: '房源名称', prob: 'houseName', width: '340px'},
           {label: '租客', prob: 'renter', width: '125px'},
           {label: '租期', prob: 'rentdate', width: '250px'},
-          {label: '每月租金', prob: 'rentmoney', width: '125px'},],
+          {label: '每月租金', prob: 'rentmoney', width: '125px'},
+        ],
+
+//        headerData:this.allAccountHeaderData,
+        unusedAccountHeaderData: [
+          {label: '房源名称', prob: 'houseName', width: '340px'},
+          {label: '租客', prob: 'renter', width: '125px'},
+          {label: '', prob: 'rentdate', width: '250px'},
+          {label: '', prob: 'rentmoney', width: '125px'},
+        ],
+        outDateHeaderData: [
+          {label: '房源名称', prob: 'houseName', width: '340px'},
+          {label: '逾期时间', prob: 'outdateLength', width: '125px'},
+          {label: '交租日期', prob: 'rentdate', width: '250px'},
+          {label: '逾期金额', prob: 'outdateMoney', width: '125px'},],
         tableData: [
-          {houseName: '都市名园D栋26G', renter: '李一飞', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元',id:'1'},
-          {houseName: '都市名园F栋27K', renter: null, isUnused: true,id:'2'},
-          {houseName: '都市名园D栋26A', renter: '赵磊', rentdate: '2017/12/01-2018/11/30', rentmoney: '215000元',id:'3'},
-          {houseName: '都市名园D栋16G', renter: '刘大磊鹏', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元',id:'4'},
-          {houseName: '阳光棕榈园10单元2008', renter: '李一飞', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元',id:'5'},],
+          {houseName: '都市名园D栋26G', renter: '李一飞', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元', id: '1'},
+          {houseName: '都市名园F栋27K', renter: null, isUnused: true, id: '2'},
+          {houseName: '都市名园D栋26A', renter: '赵磊', rentdate: '2017/12/01-2018/11/30', rentmoney: '215000元', id: '3'},
+          {houseName: '都市名园D栋16G', renter: '刘大磊鹏', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元', id: '4'},
+          {houseName: '阳光棕榈园10单元2008', renter: '李一飞', rentdate: '2017/12/01-2018/11/30', rentmoney: '5000元', id: '5'},],
       }
     },
     methods: {
-      addRenter:function () {
+      addRenter: function () {
         let houseId = 'sss'
-        MyUtil.linkToPath(this,'/renter/add?houseId='+houseId)
+        MyUtil.linkToPath(this, '/renter/add?houseId=' + houseId)
       },
-      toRenterDetail:function () {
+      toRenterDetail: function () {
         let houseId = 'sss'
-        MyUtil.linkToPath(this,'/renter/detail?houseId='+houseId)
+        MyUtil.linkToPath(this, '/renter/detail?houseId=' + houseId)
       },
-      editAccount:function () {
+      editAccount: function () {
         let houseId = 'sss'
-        MyUtil.linkToPath(this,'/account/edit?houseId='+houseId)
+        MyUtil.linkToPath(this, '/account/edit?houseId=' + houseId)
       },
-      deleteAccount:function () {
+      deleteAccount: function () {
         console.log('deleteAccount')
       },
 
-      deleteAccountConfim:function (deleteAccountId) {
-        console.log('deleteAccountId = '+deleteAccountId)
-        this.deleteId = deleteAccountId
-      }
     }
   }
 
@@ -191,15 +159,14 @@
           color: #999999;
         }
 
-        .more-action{
-          color:#2E8AE6;
+        .more-action {
+          color: #2E8AE6;
           font-size: 18px;
           cursor: pointer;
           margin-right: 30px;
-          &:hover,&:active,&:focus{
+          &:hover, &:active, &:focus {
             background-color: white;
           }
-
 
         }
 
@@ -210,3 +177,39 @@
 
 
 </style>
+
+
+<!--:title="item.houseName"-->
+<!--v-model="more_action_popover_visible"-->
+<!--width="200"-->
+<!--content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"-->
+<!--<el-popover-->
+<!--placement="top"-->
+<!--width="150"-->
+<!--v-model="item.id === deleteId"-->
+<!--trigger="click">-->
+<!--<div style="text-align: center; margin: auto">-->
+<!--<el-button size="mini" type="text" @click="deleteId = deleteIdDefault">取消</el-button>-->
+<!--<el-button type="primary" size="mini" @click="deleteId = deleteIdDefault">确定</el-button>-->
+<!--</div>-->
+<!--<el-button slot="reference" @click="deleteAccountConfim(item.id)">删除{{item.id}}</el-button>-->
+<!--</el-popover>-->
+
+<!--<el-popover-->
+<!--placement="bottom"-->
+<!--width="150"-->
+<!--v-model='item.id === menu_more_id'>-->
+<!--<div slot="reference" @mouseenter="toShowMenuMore(item.id)">-->
+
+<!--<el-button class="more-action"  @click="clickMore(item.id)" >更多</el-button>-->
+<!--</div>-->
+<!--<el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;"  @click="editAccount">编辑账本</el-button>-->
+<!--<el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="toRenterDetail">租客详情</el-button>-->
+
+
+<!--<el-button style="display: block;width: 150px;margin: 5px auto;font-size: 16px;" @click="deleteAccountConfim(item.id)">删除账本</el-button>-->
+<!--<div style="text-align: center; margin: auto" v-show="item.id === deleteId">-->
+<!--<el-button size="mini" type="primary" @click="deleteId = deleteIdDefault">取消</el-button>-->
+<!--<el-button type="primary" size="mini" @click="deleteId = deleteIdDefault">确定</el-button>-->
+<!--</div>-->
+<!--</el-popover>-->
